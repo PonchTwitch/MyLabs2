b11d0b20061227c6a02b69158a9944d68e86db81
Юнус Салманов <tu2337@gmail.com>
2023-10-11 17:06:04 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -176,3 +176,7 @@ def example():
     ]
 
     return render_template('example.html', name=name, labnum=labnum, coursenum=coursenum, groupnum=groupnum, fruits=fruits, books=books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
